6909937f1ba80ab2a5a61e37f17a94fe0b97af0d
Levi <lnatamm25@gmail.com>
2023-02-16 15:40:51 -0300

    Questão 1029 do beecrowd realizada

---
+++ Exercicios/src/FibonacciQuantasChamadas/Main.java
@@ -0,0 +1,57 @@
+package FibonacciQuantasChamadas;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static int x;
+
+    public static int fib(int n){
+
+        if(n == 0){
+
+            return 0;
+
+        }
+
+        else if(n == 1){
+
+            return 1;
+
+        }
+
+        else if(n > 1){
+
+            x+=2;
+            return fib(n - 1) + fib(n - 2);
+
+        }
+
+        else {
+
+            return -1;
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.next());
+
+        for(int i = 0; i < n; i++){
+
+            x = 0;
+
+            int k = Integer.parseInt(sc.next());
+            int r = fib(k);
+
+            System.out.println(String.format("fib(%d) = %d calls = %d", k, x, r));
+
+        }
+
+    }
+
+}
