6e6590982ae31c4548cd67d720f12dd2dcce6e7f
Levi <lnatamm25@gmail.com>
2023-03-14 11:45:28 -0300

    Questões 1097, 1098 e 1099 do beecrowd realizadas

---
+++ Exercicios/src/Sequencia/IJ3.java
@@ -0,0 +1,19 @@
+package Sequencia;
+
+public class IJ3 { //Mudar para "Main"
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 9; i+=2){
+
+            for(int j = i + 6; j > i + 3; j--){
+
+                System.out.println("I=" + i + " " + "J=" + j);
+
+            }
+
+        }
+
+    }
+
+}
---
+++ Exercicios/src/Sequencia/IJ4.java
@@ -0,0 +1,21 @@
+package Sequencia;
+
+import java.text.DecimalFormat;
+
+public class IJ4 { //Mudar para "Main"
+
+    public static void main(String[] args) {
+
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+        for (Double i = 0.0; i < 2; i += 0.2) {
+            for (Double j = i + 1.0; j <= i + 3.0; j++) {
+                String formattedI = decimalFormat.format(i);
+                String formattedJ = decimalFormat.format(j);
+                System.out.printf("I=%s J=%s\n", formattedI, formattedJ);
+            }
+        }
+
+    }
+
+}
