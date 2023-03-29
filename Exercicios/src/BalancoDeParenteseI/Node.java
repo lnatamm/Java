2b39d275be1c4ef20856b80bcb4956c96fd2b5ea
Levi <lnatamm25@gmail.com>
2023-03-29 19:05:38 -0300

    Questão 1068 do beecrowd resolvida

---
+++ Exercicios/src/BalancoDeParenteseI/Node.java
@@ -0,0 +1,32 @@
+package BalancoDeParenteseI;
+
+public class Node<T> {
+
+    private T data;
+
+    private Node<T> prev;
+
+    public Node(T data){
+
+        this.data = data;
+        this.prev = null;
+
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public Node<T> getPrev() {
+        return prev;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
+
+}
