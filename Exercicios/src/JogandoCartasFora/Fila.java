13ed1fa2f1b38dde9467c662e538d0ea5ecfb935
Levi <lnatamm25@gmail.com>
2023-05-04 22:09:06 -0300

    Questão 1110 do beecrowd resolvida

--- Exercicios/src/JogandoCartasFora/Fila.java
+++ Exercicios/src/JogandoCartasFora/Fila.java
@@ -1,4 +1,282 @@
 package JogandoCartasFora;
 
-public class Fila {
+import java.util.EmptyStackException;
+import java.util.Iterator;
+
+public class Fila<T> implements Iterable<T>{
+
+    @Override
+    public Iterator<T> iterator() {
+        return new FilaIterator();
+    }
+
+    private class FilaIterator implements Iterator<T> {
+
+        private Node<T> curr;
+
+        public FilaIterator(){
+
+            curr = first;
+
+        }
+        @Override
+        public boolean hasNext() {
+            return curr != null;
+        }
+
+        @Override
+        public T next() {
+            T data = curr.getData();
+            curr = curr.getNext();
+            return data;
+        }
+
+    }
+
+    private Node<T> first;
+
+    private Node<T> last;
+
+    private int cont;
+
+    public Fila(){
+
+        first = null;
+        last = null;
+        cont = 0;
+
+    }
+
+    public int size(){
+
+        return cont;
+
+    }
+
+    public void enqueue(T data){
+
+        Node<T> node = new Node<>(data);
+
+        if(first == null){
+
+            first = node;
+            last = node;
+
+        }
+
+        else {
+
+            node.setPrev(last);
+            last.setNext(node);
+            last = node;
+
+        }
+
+        cont++;
+
+    }
+
+    public T dequeue(){
+
+        Node<T> node;
+
+        if(first == null){
+
+            throw new EmptyStackException();
+
+        }
+
+        else {
+
+            node = first;
+            first = first.getNext();
+            try {
+                first.setPrev(null);
+            }
+            catch (NullPointerException e){
+                first = null;
+                last = null;
+            }
+
+            cont--;
+            return node.getData();
+
+        }
+
+    }
+
+    public T dequeue(int i){
+
+        Node<T> node;
+
+        if(first == null){
+
+            throw new EmptyStackException();
+
+        }
+
+        else {
+
+            if (i == 0) {
+
+                node = first;
+                first = first.getNext();
+                try {
+                    first.setPrev(null);
+                }
+                catch (NullPointerException e){
+                    first = null;
+                    last = null;
+                }
+
+                cont--;
+                return node.getData();
+
+            }
+
+            else if (i == cont - 1) {
+
+                node = last;
+                last = last.getPrev();
+                try{
+                    last.setNext(null);
+                }
+                catch (NullPointerException e){
+                    first = null;
+                    last = null;
+                }
+
+                cont--;
+                return node.getData();
+
+            }
+
+            else {
+
+                Node<T> aux = first;
+
+                for(int j = 0; j < i; j++){
+
+                    aux = aux.getNext();
+
+                }
+
+                node = aux;
+                aux.getPrev().setNext(aux.getNext());
+
+            }
+
+            return node.getData();
+
+        }
+
+    }
+
+    public T dequeue(T n){
+
+        Node<T> node;
+
+        if(first == null){
+
+            throw new EmptyStackException();
+
+        }
+
+        else {
+
+            if (n.equals(first.getData())) {
+
+                node = first;
+                first = first.getNext();
+                try {
+                    first.setPrev(null);
+                }
+                catch (NullPointerException e){
+                    first = null;
+                    last = null;
+                }
+
+                cont--;
+                return node.getData();
+
+            }
+
+            else if (n.equals(last.getData())) {
+
+                node = last;
+                last = last.getPrev();
+                try{
+                    last.setNext(null);
+                }
+                catch (NullPointerException e){
+                    first = null;
+                    last = null;
+                }
+
+                cont--;
+                return node.getData();
+
+            }
+
+            else {
+
+                Node<T> aux = first;
+
+                while (!aux.getData().equals(n)){
+
+                    aux = aux.getNext();
+
+                }
+
+                node = aux;
+                aux.getPrev().setNext(aux.getNext());
+                cont--;
+                return node.getData();
+
+            }
+
+        }
+
+    }
+
+    public T peek(){
+
+        if(first == null){
+
+            throw new EmptyStackException();
+
+        }
+
+        else {
+
+            return first.getData();
+
+        }
+
+    }
+
+    @Override
+    public String toString(){
+
+        String s = "[";
+
+        Node<T> curr = first;
+
+        if(first != null) {
+
+            while (curr.getNext() != null) {
+
+                s += curr.getData() + " ";
+                curr = curr.getNext();
+
+            }
+
+            return s + curr.getData() + "]";
+
+        }
+
+        return "";
+
+    }
+
 }
--- Exercicios/src/JogandoCartasFora/Main.java
+++ Exercicios/src/JogandoCartasFora/Main.java
@@ -1,4 +1,40 @@
 package JogandoCartasFora;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+
+        while(n != 0) {
+
+            Fila<Integer> baralho = new Fila<>();
+
+            for (int i = 1; i <= n; i++) {
+
+                baralho.enqueue(i);
+
+            }
+
+            System.out.print("Discarded cards: " + baralho.dequeue());
+
+            while(baralho.size() >= 2){
+
+                baralho.enqueue(baralho.dequeue());
+                System.out.print(", " + baralho.dequeue());
+
+            }
+
+            System.out.println("\nRemaining card: " + baralho.peek());
+
+            n = Integer.parseInt(sc.nextLine());
+
+        }
+
+    }
+
 }
